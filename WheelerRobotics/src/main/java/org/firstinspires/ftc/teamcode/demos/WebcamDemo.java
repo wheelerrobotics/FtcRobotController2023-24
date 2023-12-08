0f57e159572a3d458375e080de43c452230344d2
danieldb <danielduartebaird@gmail.com>
2023-12-08 15:42:47 -0500

    before stash recovery

--- WheelerRobotics/src/main/java/org/firstinspires/ftc/teamcode/demos/WebcamDemo.java
+++ WheelerRobotics/src/main/java/org/firstinspires/ftc/teamcode/demos/WebcamDemo.java
@@ -3,20 +3,22 @@ package org.firstinspires.ftc.teamcode.demos;
 import static org.firstinspires.ftc.teamcode.helpers.RelativePoseFinder.findPose;
 
 import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
-import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.teamcode.helpers.AprilDet;
+import org.firstinspires.ftc.teamcode.helpers.apriltag.Globalpositioning;
+import org.firstinspires.ftc.teamcode.robot.boats.Odo;
 import org.firstinspires.ftc.teamcode.vision.BotVision;
 import org.firstinspires.ftc.vision.apriltag.AprilTagPoseFtc;
 import org.openftc.apriltag.AprilTagDetection;
 
 import java.util.ArrayList;
+
+import java.util.concurrent.TimeUnit;
 
 @TeleOp
 public class WebcamDemo extends LinearOpMode {
@@ -24,10 +26,23 @@ public class WebcamDemo extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         Telemetry tele = FtcDashboard.getInstance().getTelemetry();
+        Odo o = new Odo();
         // simple class for viewing camera feed and testing processors
         AprilDet ad = new AprilDet();
         ad.init(hardwareMap, "Webcam 1");
         waitForStart();
+
+
+        while (opModeIsActive()) {
+            o.motorDriveXYVectors(gamepad1.);
+            ArrayList<AprilTagDetection> dets = ad.getDetected();
+            if (dets == null) continue;
+            for (AprilTagDetection i : dets) {
+                global_position gp = Globalpositioning.find_gglobal_pose(i);
+
+            }
+
+        }
         while (opModeIsActive()){
             try{
                 ArrayList<AprilTagDetection> dets = ad.getDetected();
@@ -47,26 +62,7 @@ public class WebcamDemo extends LinearOpMode {
                         tele.addData("pose secondAngle", orientation.secondAngle); // updown
                         tele.addData("pose thirdAngle", orientation.thirdAngle); // roll
 
-                        AprilTagPoseFtc ftcDet = findPose(i);
-                        tele.addData("supposed x", ftcDet.x);
-                        tele.addData("supposed y", ftcDet.y);
-                        tele.addData("supposed z", ftcDet.z);
-                        tele.addData("supposed roll", ftcDet.roll);
-                        tele.addData("supposed pitch", ftcDet.pitch);
-                        tele.addData("supposed yaw", ftcDet.yaw);
-                        tele.addData("supposed range", ftcDet.range * 1/0.0254); //53.75/0.533 far 34/0.344
-                        tele.addData("supposed x offset", (ftcDet.range * 1/0.0254) * Math.cos(ftcDet.elevation) * Math.cos(ftcDet.yaw));
-                        tele.addData("supposed z offset", (ftcDet.range * 1/0.0254) * Math.cos(ftcDet.elevation) * Math.sin(ftcDet.yaw));
-                        tele.addData("supposed bearing", ftcDet.bearing);
-                        tele.addData("supposed elevation", ftcDet.elevation);
-                        tele.update();
-                    }
-            }
-            }
-            catch (Exception e) {
-                tele.addData("ERR!", e.getMessage());
-                tele.update();
-            }
-        }
+
+
     }
 }
---
+++ WheelerRobotics/src/main/java/org/firstinspires/ftc/teamcode/helpers/apriltag/Globalpositioning.java
@@ -0,0 +1,91 @@
+package org.firstinspires.ftc.teamcode.helpers.apriltag;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.vision.apriltag.AprilTagPoseFtc;
+import org.openftc.apriltag.AprilTagDetection;
+
+public class Globalpositioning {
+
+    public static AngleUnit outputUnitsAngle = AngleUnit.RADIANS;
+
+    public Globalpositioning() { // learn this syntax
+
+    }
+
+    public static Pose2d globalPositionToPose(global_position position) {
+        return new Pose2d(position.global_x, position.global_y, position.rotation_z);
+    }
+
+    public static AprilTagPoseFtc findPose(AprilTagDetection detection) {
+        AprilTagPoseFtc ftcPose = new AprilTagPoseFtc();
+
+        ftcPose.x = detection.pose.x;
+        ftcPose.y = detection.pose.z;
+        ftcPose.z = -detection.pose.y;
+
+        Orientation rot = Orientation.getOrientation(detection.pose.R, AxesReference.INTRINSIC, AxesOrder.YXZ,
+                outputUnitsAngle);
+        ftcPose.yaw = -rot.firstAngle;
+        ftcPose.roll = rot.thirdAngle;
+        ftcPose.pitch = rot.secondAngle;
+
+        ftcPose.range = Math.hypot(ftcPose.x, ftcPose.y);
+        ftcPose.bearing = outputUnitsAngle.fromUnit(AngleUnit.RADIANS, Math.atan2(-ftcPose.x, ftcPose.y));
+        ftcPose.elevation = outputUnitsAngle.fromUnit(AngleUnit.RADIANS, Math.atan2(ftcPose.z, ftcPose.y));
+
+        return ftcPose;
+    }
+
+    public static global_position find_global_pose(AprilTagDetection detection) // get id from findPose static,
+    {
+
+        AprilTagPoseFtc ftcPose = findPose(detection);
+        double mounted_z_cam = .3; // include more here
+        global_position bot_position = new global_position();
+        global_position tag_detected = new global_position();
+        double detected_id = detection.id;
+
+        if (detected_id == tag_positions.left_tag_blue.id) {
+            tag_detected = tag_positions.left_tag_blue;
+        }
+        if (detected_id == tag_positions.right_tag_blue.id) {
+            tag_detected = tag_positions.right_tag_blue;
+        }
+        if (detected_id == tag_positions.left_tag_red.id) {
+            tag_detected = tag_positions.left_tag_red;
+        }
+        if (detected_id == tag_positions.right_tag_red.id) {
+            tag_detected = tag_positions.right_tag_red;
+        }
+        if (detected_id == tag_positions.center_tag_red.id) {
+            tag_detected = tag_positions.center_tag_red;
+        }
+        if (detected_id == tag_positions.center_tag_blue.id) {
+            tag_detected = tag_positions.center_tag_blue;
+        }
+        if (detected_id == tag_positions.wall_tag_left.id) {
+            tag_detected = tag_positions.wall_tag_left;
+        }
+        if (detected_id == tag_positions.wall_tag_right.id) {
+            tag_detected = tag_positions.wall_tag_right;
+        }
+
+        bot_position.global_y = (tag_detected.global_y - (ftcPose.range * 1/0.0254) * Math.cos(ftcPose.elevation) * Math.sin(ftcPose.yaw));
+        bot_position.global_x = (tag_detected.global_x - (ftcPose.range * 1/0.0254) * Math.cos(ftcPose.elevation) * Math.cos(ftcPose.yaw));
+
+        bot_position.global_z = (mounted_z_cam - (tag_detected.global_z + (ftcPose.range * 1/0.0254) * Math.sin(ftcPose.elevation))); // is this right?
+        bot_position.rotation_z = (tag_detected.rotation_z + ftcPose.yaw);
+        bot_position.rotation_y = (tag_detected.rotation_y + ftcPose.roll);
+        bot_position.rotation_x = (tag_detected.rotation_x + ftcPose.pitch);
+
+        bot_position.id = 252;
+
+        return bot_position;
+    }
+
+}
